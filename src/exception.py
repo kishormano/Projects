20cde8120de7c90d143534cc76329da01f60f161
kishor <kishormanohar.14@gmail.com>
2024-08-30 23:30:33 +0530

    log creation changes

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def err_msg_det(err,error_detail:sys):
 	_,_,exc_tb = error_detail.exc_info()
@@ -13,10 +13,12 @@ class CustomException(Exception):
 
 	def __str__(self):
 		return self.error_message
-##if __name__ == "__main__":
-##	try:
-##		a=1/0
-##	except Exception as e:
-##		logging.info("division by zero error")
-##		raise CustomException(e,sys) 
+
+
+if __name__ == "__main__":
+	try:
+		a=1/0
+	except Exception as e:
+		logging.info("division by zero error")
+		raise CustomException(e,sys) 
 
